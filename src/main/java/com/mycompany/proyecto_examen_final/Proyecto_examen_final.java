41faccabd427bd3836be5101982c58bce540aab6
drax3l <alexjeanpierquispesierra@gmail.com>
2025-02-01 10:46:09 -0500

    realizar commit primer inicio

---
+++ src/main/java/com/mycompany/proyecto_examen_final/Proyecto_examen_final.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_examen_final;
+
+/**
+ *
+ * @author usuario
+ */
+public class Proyecto_examen_final {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
